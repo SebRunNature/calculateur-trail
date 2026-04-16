5b12f804018a18d733c25db422d8dc9867661c20
Seb Run Nature <seb.salomon.lux@gmail.com>
2026-04-16 17:06:28 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'srn-calculateur-trail-v1';
+const FILES = [
+  '/calculateur-trail/',
+  '/calculateur-trail/index.html',
+  '/calculateur-trail/manifest.json'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(FILES);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
